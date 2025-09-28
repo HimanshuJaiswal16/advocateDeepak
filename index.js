e0109c9f662dbf66efcddefaa1f9840703419711
Himanshu Jaiswal <GI0328@garudaAD.com>
2025-09-28 16:24:55 +0530

    initital commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// FAQ toggle
+document.querySelectorAll(".faq-question").forEach(button => {
+    button.addEventListener("click", () => {
+        const answer = button.nextElementSibling;
+        answer.style.display = answer.style.display === "block" ? "none" : "block";
+    });
+});
+
+  document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+      const faqItem = button.parentElement;
+      faqItem.classList.toggle('active');
+    });
+  });
