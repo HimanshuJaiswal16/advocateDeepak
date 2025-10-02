56dea37b7cad8672bc91464ec113e3d45f78473f
Himanshu Jaiswal <GI0328@garudaAD.com>
2025-10-02 18:53:27 +0530

    all changes made

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // FAQ toggle
 document.querySelectorAll(".faq-question").forEach(button => {
-    button.addEventListener("click", () => {
-        const answer = button.nextElementSibling;
-        answer.style.display = answer.style.display === "block" ? "none" : "block";
-    });
+  button.addEventListener("click", () => {
+    const answer = button.nextElementSibling;
+    answer.style.display = answer.style.display === "block" ? "none" : "block";
+  });
 });
 
-  document.querySelectorAll('.faq-question').forEach(button => {
-    button.addEventListener('click', () => {
-      const faqItem = button.parentElement;
-      faqItem.classList.toggle('active');
-    });
+document.querySelectorAll('.faq-question').forEach(button => {
+  button.addEventListener('click', () => {
+    const faqItem = button.parentElement;
+    faqItem.classList.toggle('active');
   });
+});
