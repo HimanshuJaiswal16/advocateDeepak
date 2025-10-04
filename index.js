63f85af1dd35f12a46e56a6c346cef296c667878
AMD <aayushtanwar@gmail.com>
2025-10-04 18:47:53 +0530

    more changes added

--- index.js
+++ index.js
@@ -12,3 +12,25 @@ document.querySelectorAll('.faq-question').forEach(button => {
     faqItem.classList.toggle('active');
   });
 });
+
+$(document).ready(function () {
+  $('.slider').slick({
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,
+          centerPadding: '0px'
+        }
+      }
+    ]
+  });
+});
